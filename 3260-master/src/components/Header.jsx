c17110f8e2b2d532ca76de3945120e8328b63752
ROCHA LEON FRANCISCO <leonrocha14@gmail.com>
2023-07-28 19:54:35 -0300

    colores al header

--- 3260-master/src/components/Header.jsx
+++ 3260-master/src/components/Header.jsx
@@ -7,6 +7,7 @@ import ModalLogin from '../components/Modal_login';
 
 const Header = () => {
   const [searchText, setSearchText] = useState('');
+  const [selectedItem, setSelectedItem] = useState(null); // Estado para almacenar el ítem seleccionado
 
   const handleSearch = () => {
     // Realiza la lógica de búsqueda utilizando el valor de searchText
@@ -17,7 +18,10 @@ const Header = () => {
     setSearchText(event.target.value);
   };
 
-  
+  const handleItemClick = (item) => {
+    setSelectedItem(item); // Actualiza el estado con el ítem seleccionado
+  };
+
   return (
     <header className="header">
       <div className="header-left">
@@ -28,24 +32,49 @@ const Header = () => {
         </div>
         <nav className="header-nav">
           <ul className="nav-list">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/locales">Locales</Link></li>
-            <li><Link to="/eventos">Eventos</Link></li>
-            <li><Link to="/contactos">Contacto</Link></li>
+            <li
+              onClick={() => handleItemClick('inicio')}
+              className={selectedItem === 'inicio' ? 'selected' : ''}
+            >
+              <Link to="/">Inicio</Link>
+            </li>
+            <li
+              onClick={() => handleItemClick('locales')}
+              className={selectedItem === 'locales' ? 'selected' : ''}
+            >
+              <Link to="/locales">Locales</Link>
+            </li>
+            <li
+              onClick={() => handleItemClick('eventos')}
+              className={selectedItem === 'eventos' ? 'selected' : ''}
+            >
+              <Link to="/eventos">Eventos</Link>
+            </li>
+            <li
+              onClick={() => handleItemClick('contacto')}
+              className={selectedItem === 'contacto' ? 'selected' : ''}
+            >
+              <Link to="/contactos">Contacto</Link>
+            </li>
           </ul>
         </nav>
       </div>
 
       <div className="header-right">
         <div className="search-container">
-          <input type="text" placeholder="Busque un Negocio o Evento" value={searchText} onChange={handleInputChange} className="search-input"
-          onKeyDown={handleSearch} />
+          <input
+            type="text"
+            placeholder="Busque un Negocio o Evento"
+            value={searchText}
+            onChange={handleInputChange}
+            className="search-input"
+            onKeyDown={handleSearch}
+          />
           <IoMdSearch size={40} className="search-icon" onClick={handleSearch} />
         </div>
         <div className="icon-container">
-          <ModalLogin/>
+          <ModalLogin />
         </div>
-
       </div>
     </header>
   );
