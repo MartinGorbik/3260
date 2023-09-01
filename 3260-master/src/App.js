1d15750b06e09d631fe2c42a01107a56c0c6cb40
MartinGorbik <martingorbik2011@gmail.com>
2023-09-01 12:06:49 -0300

    Agrego integracion de mercado pago para frontend

--- 3260-master/src/App.js
+++ 3260-master/src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-
-import Footer from './components/Footer';
-import Banner from './components/Banner';
-import Recommended from './components/Recommended';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contacto from './components/Contacto';
 import Home from './pages/Home';
 import Locales from './pages/Locales';
 import Eventos from './pages/Eventos';
@@ -18,7 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path='/' Component={Home}></Route>
-          {/* <Route exact path='/locales' Component={Locales}></Route> */}
+          <Route exact path='/locales' Component={Locales}></Route>
           <Route exact path='/eventos' Component={Eventos}></Route>
           <Route exact path='/contactos' Component={Contactos}></Route>
         </Routes>
--- 3260-master/src/pages/Locales.js
+++ 3260-master/src/pages/Locales.js
@@ -1,11 +1,43 @@
 import React from 'react';
+/* import axios from 'axios'; */
+import { Wallet, initMercadoPago } from '@mercadopago/sdk-react';
+import { useState } from 'react';
 
 function Test() {
+  /* const [preferenceId, setPreferenceId] = useState(null);
+  initMercadoPago('YOUR_PUBLIC_KEY');
+
+  const createPreference = async () => {
+    try{
+      const response = await axios.post("http://[::1]:3000/", {
+      description: "local",
+      price: 100,
+      quantity: 1,
+      currency_id: "ARS"
+      });
+
+      const {id} = response.data;
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBuy = async () => {
+    const id = await createPreference();
+    if (id) {
+      setPreferenceId(id);
+    }
+  }; */
+
   return (
     <div>
       <h1>
         Test de LOCALES
       </h1>
+      {/* <p className='price'>$500</p>
+        <button onClick={handleBuy} >Comprar</button>
+      {preferenceId && <Wallet initialization={{ preferenceId: "<PREFERENCE_ID>"}} />} */}
     </div>
   );
 }
